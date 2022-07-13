dedb18aa6f7144bf1ac26485fb92dc3a71057f9e
Essien-SM <stephenyessien@gmail.com>
2022-07-13 11:08:51 +0000

    including header and footer on pages

---
+++ partials/header.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.98.0">
+    <title>Paul-Timothy Leadership Network Inc.</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/carousel/">
+    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/dist/css/carousel.css" rel="stylesheet">
+    <link href="assets/dist/css/contact.css">
+
+    <!-- googel font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href='https://fonts.googleapis.com/css?family=PT Sans Narrow|Staatliches|Sofia|Raleway|Tangerine|Acme|Alex Brush|Alike|Anaheim|Amita|Pacifico|Seaweed Script|Sacramento|Roboto|Open+Sans Swash Caps' rel='stylesheet'>
+
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <script src="https://kit.fontawesome.com/c493b6cb9b.js" crossorigin="anonymous"></script>
+
+  </head>
+  <body>  
+    <header>
+      <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.html"> <img src="assets/img/Picture1.png" alt="" width="70" height="40"> </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+              <li class="nav-item">
+                <a class="nav-link" href="be_part_of_us.html">| Be part of Us |</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#">| Donate Now |</a>
+              </li>
+              <!-- <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+              </li> -->
+            </ul>
+            <ul class="navbar-nav">
+            <li>
+              <a class="nav-link active fst-italic fw-bold" aria-current="page" href="index.html">Home</a>
+            </li>
+            <li>
+              <a class="nav-link" aria-current="page" href="aboutus.html">About Us</a>
+            </li>
+            <li>
+              <a class="nav-link" aria-current="page" href="news.html">News</a>
+            </li>
+            <li>
+              <a class="nav-link" aria-current="page" href="stories.html">Stories</a>
+            </li>
+            <li>
+              <a class="nav-link" aria-current="page" href="events.html">Events</a>
+            </li>
+            <!-- <li>
+              <a class="nav-link" aria-current="page" href="resource_library.html">Resources Librarily</a>
+            </li> -->
+            <li class="nav-item" style="cursor: pointer;">
+              <div class="dropdown">
+                <a class="nav-link dropdown-toggle" id="dropdownMenuButtonSM" data-bs-toggle="dropdown" aria-expanded="false">
+                  Resources Library
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButtonSM">
+                  <li><a class="dropdown-item" href="resource_library.html">Video Library</a></li>
+                  <li><a class="dropdown-item" href="motivational_page.html">Motivational quotes</a></li>
+                </ul>
+              </div>             
+            </li>
+            <li>
+              <a class="nav-link" aria-current="page" href="contact.html">Contact Us</a>
+            </li>
+
+          </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
